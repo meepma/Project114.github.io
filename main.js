076d02d4e2dfcff0807fbdbc47401d231bab1931
meepma <80076185+meepma@users.noreply.github.com>
2021-11-16 18:54:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+
+nosex=0;
+nosey=0;
+
+
+
+
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(400,400);
+canvas.center();
+video= createCapture(VIDEO);
+video.hide();
+
+poseNet= ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+image(video,0,0,400,400)
+}
+
+function takeSnapShot(){
+    save("Junk.png")
+}
+
+function modelLoaded(){
+    console.log("poseNet is loaded");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+    }
+}
